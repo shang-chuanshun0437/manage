bf7b2392ef3bfcc0e07e4fda7fbc21ec685a72a9
shangchuanshun <519983770@qq.com>
2018-12-14 23:46:09 +0800

    edfef

--- src/axios/instance.js
+++ src/axios/instance.js
@@ -11,15 +11,15 @@ import * as util from "@/utils/index";
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://127.0.0.1:8080/smartlock',
+    baseURL: 'http://192.168.1.3:8080/smartlock',
     timeout: 10000
 });
 
 instance.defaults.headers.common["Authorization"] = "Rene";
 instance.defaults.headers.post["Content-Type"] = "application/json";
 //错误处理
-// instance.interceptors.response.use(function(response) {
-//     return response;
-// }, util.catchError);
+instance.interceptors.response.use(function(response) {
+    return response;
+}, util.catchError);
 
 export { instance };
